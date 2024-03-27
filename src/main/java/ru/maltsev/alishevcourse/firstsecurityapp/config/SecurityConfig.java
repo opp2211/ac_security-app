ac88985bdfa170e6caa9d900e48b11f8c582c300
Kirill Maltsev <opp.2211@gmail.com>
2024-03-27 16:04:20 +0500

    feat: own login page + filterChain config (new style)

--- src/main/java/ru/maltsev/alishevcourse/firstsecurityapp/config/SecurityConfig.java
+++ src/main/java/ru/maltsev/alishevcourse/firstsecurityapp/config/SecurityConfig.java
@@ -3,9 +3,12 @@ package ru.maltsev.alishevcourse.firstsecurityapp.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -14,5 +17,22 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry
+                        .requestMatchers("/auth/login", "/error").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/auth/login")
+                        .loginProcessingUrl("/process_login")
+                        .defaultSuccessUrl("/hello", true)
+                        .failureUrl("/auth/login?error"));
+
+
+        return http.build();
     }
 }
---
+++ src/main/java/ru/maltsev/alishevcourse/firstsecurityapp/controllers/AuthController.java
@@ -0,0 +1,15 @@
+package ru.maltsev.alishevcourse.firstsecurityapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "auth/login";
+    }
+}
