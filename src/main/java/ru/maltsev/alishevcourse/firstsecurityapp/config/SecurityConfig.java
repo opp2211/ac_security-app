262092946d638f5d3b23b18a1e3045a6e55e1020
Kirill Maltsev <opp.2211@gmail.com>
2024-03-27 14:57:06 +0500

    feat: -AuthenticationProviderImpl +NoOpPasswordEncoder

--- src/main/java/ru/maltsev/alishevcourse/firstsecurityapp/config/SecurityConfig.java
+++ src/main/java/ru/maltsev/alishevcourse/firstsecurityapp/config/SecurityConfig.java
@@ -1,11 +1,18 @@
 package ru.maltsev.alishevcourse.firstsecurityapp.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
