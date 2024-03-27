d1ddfd31d7c85c235ab7a67c6a0b8da4becb959c
Kirill Maltsev <opp.2211@gmail.com>
2024-03-28 03:03:15 +0500

    feat: encryption

--- src/main/java/ru/maltsev/alishevcourse/firstsecurityapp/config/SecurityConfig.java
+++ src/main/java/ru/maltsev/alishevcourse/firstsecurityapp/config/SecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -16,7 +16,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public PasswordEncoder getPasswordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
--- src/main/java/ru/maltsev/alishevcourse/firstsecurityapp/services/PeopleService.java
+++ src/main/java/ru/maltsev/alishevcourse/firstsecurityapp/services/PeopleService.java
@@ -1,6 +1,7 @@
 package ru.maltsev.alishevcourse.firstsecurityapp.services;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.maltsev.alishevcourse.firstsecurityapp.models.Person;
@@ -11,6 +12,7 @@ import ru.maltsev.alishevcourse.firstsecurityapp.repositories.PeopleRepo;
 @Transactional(readOnly = true)
 public class PeopleService {
     private final PeopleRepo peopleRepo;
+    private final PasswordEncoder passwordEncoder;
 
     public boolean existByUsername(String username) {
         return peopleRepo.existsByUsername(username);
@@ -18,6 +20,7 @@ public class PeopleService {
 
     @Transactional
     public void registerNew(Person person) {
+        person.setPassword(passwordEncoder.encode(person.getPassword()));
         peopleRepo.save(person);
     }
 }
